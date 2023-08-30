46208b10c88d749bbf92b1ea3d31d558b11e7b14
nea <ziplies@gmail.com>
2023-08-30 18:45:29 +0200

    feat: Job cleanup

--- src/Jobs/PastableJob.php
+++ src/Jobs/PastableJob.php
@@ -2,18 +2,19 @@
 
 namespace ElipZis\Pastable\Jobs;
 
+use ElipZis\Pastable\Helper\PastableLogger;
 use ElipZis\Pastable\Models\Traits\CopyPastable;
+use ElipZis\Pastable\Models\Traits\CutPastable;
 use Illuminate\Bus\Queueable;
-use Illuminate\Container\Container;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
 
 class PastableJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, PastableLogger;
 
     /**
      * Create a new job instance.
@@ -28,35 +29,47 @@ class PastableJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //Get all classes implementing the CopyPastable trait
-        $classes = $this->getPastableClasses();
+        //Get all classes implementing our traits
+        $copyClasses = $this->getPastableClasses(CopyPastable::class);
+        $cutClasses = $this->getPastableClasses(CutPastable::class);
 
-        if (empty($classes)) {
-            Log::info('[CopyPasteableJob] No pasteable classes found.');
-
+        if (empty($copyClasses) && empty($cutClasses)) {
+            $this->log('No pasteable classes found.');
             return;
         }
 
-        Log::info('[CopyPasteableJob] Found '.count($classes).' pasteable classes.');
+        $count = count($copyClasses) + count($cutClasses);
+        $this->log("Found {$count} pasteable classes.");
 
-        foreach ($classes as $class) {
+        foreach ($copyClasses as $copyClass) {
             //Copy & Paste every class
-            /** @var CopyPastable $instance */
-            $instance = new $class;
-            $instance->paste();
+//            /** @var CopyPastable $instance */
+//            $instance = new $class;
+//            $instance->paste();
+        }
+
+        foreach ($cutClasses as $cutClass) {
+            //Cut & Paste every class
+//            /** @var CutPastable $instance */
+//            $instance = new $class;
+//            $instance->paste();
         }
     }
 
-    public function getPastableClasses(string $traitClass = CopyPastable::class): array
+    /**
+     * @param string $traitClass
+     * @return array
+     */
+    public function getPastableClasses(string $traitClass): array
     {
-        $appNamespace = Container::getInstance()->getNamespace();
+        $appNamespace = Application::getInstance()->getNamespace();
 
         return collect(File::allFiles(app_path()))->map(static function ($item) use ($appNamespace) {
             $rel = $item->getRelativePathName();
 
             return sprintf('%s%s', $appNamespace, implode('\\', explode('/', substr($rel, 0, strrpos($rel, '.')))));
-        })->filter(fn ($class) => class_exists($class))
-            ->filter(fn ($class) => in_array($traitClass, class_uses_recursive($class)))
+        })->filter(fn($class) => class_exists($class))
+            ->filter(fn($class) => in_array($traitClass, class_uses_recursive($class)))
             ->all();
     }
 }
